1fe6d4f84cddee780fb4eae67637f3a98173cd48
tdn <2387083001@qq.com>
2021-04-01 20:41:00 +0800

    delete log

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-/*
- * @Descripttion: 
- * @version: 1.0
- * @Author: 张泽涛
- * @LastEditors: 张泽涛
- * @LastEditTime: 2021-04-01 20:37:15
- */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
