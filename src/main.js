ab5bf9941c879a3280fd0914a0aeb9f4b498a4b1
tdn <2387083001@qq.com>
2021-04-01 20:37:32 +0800

    trim

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@
  * @version: 1.0
  * @Author: 张泽涛
  * @LastEditors: 张泽涛
- * @LastEditTime: 2021-04-01 20:29:30
+ * @LastEditTime: 2021-04-01 20:37:15
  */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 
 createApp(App).use(store).use(router).mount('#app')
