904259e2c7c90a134e8aa3f0dc1f1915bd7c063a
tdn <2387083001@qq.com>
2021-04-01 20:29:43 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 张泽涛
+ * @LastEditors: 张泽涛
+ * @LastEditTime: 2021-04-01 20:29:30
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 
 createApp(App).use(store).use(router).mount('#app')
